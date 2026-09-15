80f0f8e526a2a36ec814d147b1021c616feebfe0
Wendell <wendell@local>
2026-09-15 17:12:54 -0300

    Usa e-mail do dono correto na isencao de licenca

--- src/lib/sales.ts
+++ src/lib/sales.ts
@@ -5,7 +5,4 @@ export const PRICE_LABEL = "R$ 37,90";
 export const CHECKOUT_URL =
   "https://checkout.wiven.com.br/checkout/cmu33jyq100h801og7wiqnr04?offer=VO27VYQ";
 
-export const LICENSE_OWNER_EMAILS: string[] = [
-  "wendellsilva673@gmail.com",
-  "atv.didaticas@gmail.com",
-];
+export const LICENSE_OWNER_EMAILS: string[] = ["atv.didaticas@gmail.com"];
